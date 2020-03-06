1f6ae80cad17a7c157a2eab3b70069f608479145
Oussama Dinia <oussama.dinia@gmail.com>
2020-03-06 09:06:48 +0000

    second endpoint

--- source/first.py
+++ source/first.py
@@ -5,9 +5,12 @@ app: FastAPI = FastAPI(debug=True)
 
 
 @app.get("/")
-async def root():
+async def index():
     return {"message": "Hello Fast Api"}
 
+@app.get("/home")
+async def home():
+    return {"message": "Home end point"}
 
 if __name__ == "__main__":
     uvicorn.run(app, host="127.0.0.1", port=8000)
